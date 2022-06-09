d644c712c9e869df1b6a20258d2a62c2089110f1
Habinger <55584446+Habinger@users.noreply.github.com>
2022-06-09 01:12:13 +0100

    initial

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value in a_dictionary.values():
+        for k in a_dictionary.copy():
+            if a_dictionary[k] == value:
+                del a_dictionary[k]
+        return a_dictionary
+    return a_dictionary
